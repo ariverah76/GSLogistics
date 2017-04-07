8ab9f29555a1d4c344d5fceec4f230d87ac584da
ariverah76 <ariverah76@hotmail.com>
2017-04-07 05:15:19 -0500

    Issue with the new grid

---
+++ GSLogisitics.Entities/GSExternalContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSLogistics.Entities
+{
+    public class GSExternalContext : DbContext
+    {
+
+        public GSExternalContext()
+            :base("GSLogisticsCentralEntities")
+        {
+
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+
+        public static GSExternalContext Create()
+        {
+            return new GSExternalContext();
+        }
+    }
+}
