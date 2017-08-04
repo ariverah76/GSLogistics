80d93f15549de2e9a6b28ed852e3adbee42504a7
Alex From Laptop <ariverah76@yahoo.com>
2017-08-04 08:16:50 -0500

    some stuff gfor

---
+++ GSLogisitics.Entities/UserCustomer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSLogistics.Entities
+{
+    [Table("UserCustomer", Schema = "dbo")]
+    public class UserCustomer
+    {
+        [Column("UserId", Order = 1)]
+        public int UserId { get; set; }
+
+        [Column("CustomerId", Order = 2)]
+        public string CustomerId { get; set; }
+
+        [Column("DivisionId", Order = 3)]
+        public int DivisionId { get; set; }
+    }
+}
